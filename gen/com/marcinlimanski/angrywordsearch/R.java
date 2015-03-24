c26eab1be1c9630369f067061212acb3c6f9b460
marcinlimanski <m.limanski@2012.hull.ac.uk>
2015-03-24 17:14:24 +0000

    New view added, user info extracted
    Activity_Reg added

--- gen/com/marcinlimanski/angrywordsearch/R.java
+++ gen/com/marcinlimanski/angrywordsearch/R.java
@@ -1648,10 +1648,13 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f09004a;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
+        public static final int btnLogIn=0x7f090040;
+        public static final int btnReg=0x7f09003f;
+        public static final int btnRegUser=0x7f090049;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
@@ -1694,6 +1697,14 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
+        public static final int tbFirstNameReg=0x7f090042;
+        public static final int tbPasswordReg=0x7f090048;
+        public static final int tbSurnameReg=0x7f090044;
+        public static final int tbUserNameReg=0x7f090046;
+        public static final int textView1=0x7f090041;
+        public static final int textView2=0x7f090043;
+        public static final int textView3=0x7f090045;
+        public static final int textView4=0x7f090047;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
@@ -1750,7 +1761,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_reg=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
