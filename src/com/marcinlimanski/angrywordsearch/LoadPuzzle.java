9c021604a39029081bdab754fe69a1ef91a560ab
marcinlimanski <m.limanski@2012.hull.ac.uk>
2015-04-08 11:32:31 +0100

    Updated the LoadPuzzle class to handle two formats, Array and Normal

--- src/com/marcinlimanski/angrywordsearch/LoadPuzzle.java
+++ src/com/marcinlimanski/angrywordsearch/LoadPuzzle.java
@@ -62,6 +62,40 @@ public class LoadPuzzle {
 			//Parsing the normal formated puzzle
 			else if(StartActivity.puzzleFormatFlag == false)
 			{
+				//Create a global JSONObject with data
+				JSONObject puzzleObject = new JSONObject(puzzle);
+				//Constructing a first array that holds two objects
+				JSONObject puzzleAndSolutionPuzzle = puzzleObject.getJSONObject("PuzzleAndSolution");
+				JSONObject targetPuzzleObject = puzzleAndSolutionPuzzle.getJSONObject("Puzzle");
+				//Checking if the object puzzle has been accessed correctly 
+				Log.i("Object accessed at index 0", targetPuzzleObject.toString());
+				
+				
+				//Extracting the Id of the puzzle 
+				playingPuzzleId = targetPuzzleObject.getString("Id");
+				//Checking if the object puzzle has been accessed correctly 
+				Log.i("Object id", playingPuzzleId.toString());
+				
+				//Extracting the words to find
+				JSONArray wordsArray = targetPuzzleObject.getJSONArray("Words");
+				for(int i=0; i<wordsArray.length(); i++){
+					wordsToFindArray.add(wordsArray.getString(i).toString());
+				}
+				
+				//Checkingif the words are parsed correctly 
+				for (String temp : wordsToFindArray){
+					Log.i("Word: ", temp);
+				}
+				
+				//Extracting the gridwords and column number
+				JSONArray gridWordsArray = targetPuzzleObject.getJSONArray("Grid");
+				//Assigning the number of columns for the grid 
+				gridNumberOfColumns = gridWordsArray.length();
+				for(int i=0; i<gridWordsArray.length(); i++){
+					
+					gridWords = gridWords + gridWordsArray.getString(i).toString();
+				}
+				
 				
 			}
 			else{
